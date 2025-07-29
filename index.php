b2f612bd2ee11b077cfc1904e917fde63250e166
Andrey Rubtsov <aa.rubtsov@icloud.com>
2025-07-29 17:56:54 +0300

    structure

--- index.php
+++ index.php
@@ -1,12 +1,56 @@
 <?php
 
-// $text = stream_get_contents(STDIN);
-// fwrite(STDOUT, $text . PHP_EOL);
-// fwrite(STDOUT, "Done" . PHP_EOL);
+
+$numDividend = null;
+
+$numDivisor = null;
 
 
-// $line = trim(fgets(STDIN)); // читает одну строку из STDIN
-// echo "вывод строки >>> " . $line;
+function numberRequest($text) {
 
-$num = fscanf(STDIN, "%d\n", $number); // читает число из STDIN
-echo "вывод числа >>> " . $num;
+    echo $text . PHP_EOL;
+    $num = fgets(STDIN);
+
+    $num = intval(trim($num));
+
+    echo gettype($num) . "\n". $num . PHP_EOL;
+
+    return $num;
+}
+
+
+function variableCheck($variable) {
+
+    if (is_int($variable)) {
+        fwrite(STDERR, "Целое число\n");
+
+        return true;
+    } elseif (is_float($variable)) {
+        fwrite(STDERR, "Дробное число\n");
+
+        return false;
+    }
+
+    fwrite(STDERR, "Это не число\n");
+
+    return false;
+}
+
+function saveVariable(){
+
+}
+
+
+function main($message) {
+    // Запрашиваем делимое
+    $request = numberRequest($message);
+
+    // Проверяем переменную на соответствие `int`
+    $validationStatus = variableCheck($request);
+    echo $validationStatus . PHP_EOL;
+}
+
+$message = "Введите делимое число";
+main($message);
+
+// echo "Проверка скрипта numDividend >>>" . $numDividend . PHP_EOL;
